0b9a7013d82c90c74245874c78a14cf28b0e5ccb
Eric Stokes <fernferret@gmail.com>
2010-12-01 09:42:55 -0500

    Added Questions

--- diversit.rb
+++ diversit.rb
@@ -7,11 +7,12 @@ require 'dm-validations'
 require 'dm-migrations'
 require 'digest/md5'
 require 'yaml'
+require 'logger'
 
 ## CONFIGURATION
 
 config = YAML::load_file("config.yml")
-
+DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, config['db_location'])
 
 ## MODELS
@@ -30,9 +31,9 @@ end
 class Question
   include DataMapper::Resource
   property :id,         Serial
-  property :body,       Text
+  property :body,       String
   property :type,       String
-  
+  property :timestamp,  DateTime
   has n, :response
 end
 
@@ -64,7 +65,21 @@ get '/' do
   haml :index
 end
 
+get '/questions' do
+  @questions = Question.all
+  haml :question_archive
+end
+
 get '/add/:user/:first/:last/:dob' do
   User.create(:username=>params[:user],:firstname=>params[:first],:lastname=>params[:last],:dob=>params[:dob])
   redirect '/'
+end
+
+get '/addquestion' do
+  haml :question_add
+end
+
+post '/addquestion' do
+  @question = Question.create(:body=>params[:question], :type=>'free')
+  haml :question_success
 end
