de3d35a1137543336775b6d9a2a8400fb58238c8
Eric Stokes <fernferret@gmail.com>
2010-12-01 08:47:26 -0500

    Started adding resource defs

--- diversit.rb
+++ diversit.rb
@@ -23,19 +23,38 @@ class User
   property :firstname,  String
   property :lastname,   String
   property :dob,        Date
+  
+  has n, :response
 end
 
 class Question
   include DataMapper::Resource
   property :id,         Serial
   property :body,       Text
+  property :type,       String
+  
+  has_n :response
 end
 
+# Our Sleep Deprived version of an interface
 class Response
   include DataMapper::Resource
   property :id,         Serial
   property :body,       Text
+  
+  belongs_to :user
+  
+  has_n :comment
+end
 
+class Answer < Response
+  belongs_to :question
+end
+
+class Comment < Response
+  belongs_to :response
+end
+  
 ## PATHS
 
 get '/' do
