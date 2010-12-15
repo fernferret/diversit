b1bf6438d5b2fcf75453dc05e0eddc3d1c86a0b9
Eric Stokes <fernferret@gmail.com>
2010-12-15 12:10:19 -0500

    Added Local image, fixed references

--- models/Question.rb
+++ models/Question.rb
@@ -9,4 +9,33 @@ class Question
   property :forday,     Date
   
   has n, :response
+  
+  def averageAge
+    age = 0
+    count = self.response.count
+    if count == 0
+      return "No Data Yet"
+    end
+    self.response.each do |resp|
+      age += resp.user.age
+    end
+    return age / count
+  end
+  
+  def commentAge
+    age = 0
+    count = 0
+    counter = {}
+    self.response.each do |resp|
+      counter = resp.commentAgeRecursive
+      age += counter['sum']
+      count += counter['count']
+    end
+    
+    if count > 0
+      return age / count
+    else
+      return "No Data Yet"
+    end
+  end
 end
--- models/Response.rb
+++ models/Response.rb
@@ -10,4 +10,16 @@ class Response
   belongs_to :user
   belongs_to :question
   is :tree, :order => :timestamp
+  
+  def commentAgeRecursive
+    countHash = Hash.new
+    countHash['sum'] = self.user.age
+    countHash['count'] = 1
+    self.children.each do |response|
+      result = response.commentAgeRecursive
+      countHash['sum'] += result['sum']
+      countHash['count'] += result['count']
+    end
+    return countHash
+  end
 end
