e8364d9b9cd95faf4e42ec90538ed38f8547c600
Kenny Gao <kennytgao@gmail.com>
2011-05-06 19:18:33 -0400

    Removed Answer and Comment from model (replaced with Response).

--- models/Answer.rb
+++
@@ -1,9 +0,0 @@
-class Answer
-  include DataMapper::Resource
-  property :id,         Serial
-  property :body,       Text
-
-  belongs_to :user
-  belongs_to :question
-  has n, :comment
-end
--- models/Comment.rb
+++
@@ -1,8 +0,0 @@
-class Comment
-  include DataMapper::Resource
-  property :id,         Serial
-  property :body,       Text
-
-  belongs_to :user
-  belongs_to :answer
-end
