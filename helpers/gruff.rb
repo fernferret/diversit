826386b0c08d527ff210e634133471f33e304a00
Kenny Gao <kennytgao@gmail.com>
2011-05-08 14:07:18 -0400

    Customized graph generation.

--- helpers/gruff.rb
+++ helpers/gruff.rb
@@ -1,6 +1,31 @@
+class DiversitPie < Gruff::Mini::Pie
+  def initialize_ivars()
+    super
+
+    @hide_title = false
+  end
+
+  def theme_diversit()
+    @green = '#339933'
+    @blue = '#336699'
+    @yellow = '#FFF804'
+    @red = '#ff0000'
+    @black = 'black'
+    @colors = [@yellow, @blue, @green, @red, @purple, @orange, @black]
+
+    self.theme = {
+      :colors => @colors,
+      :marker_color => 'white',
+      :font_color => 'white',
+      :background_color => 'transparent'
+    }
+  end
+end
+
 def gruff_pie(title, data, qid, demo_id)
-  g = Gruff::Pie.new
+  g = DiversitPie.new
   g.title = title
+  g.theme_diversit()
   data.each do |d|
     g.data d[0], d[1]
   end
